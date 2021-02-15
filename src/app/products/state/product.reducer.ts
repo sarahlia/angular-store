a48882969c75e1e0e3498832a955ec4868db20ad
sarahlia <sarahn@rightnowmedia.org>
2021-02-15 17:59:07 -0600

    subscribed to the Store to get state changes.

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -3,7 +3,6 @@ import {createAction, createReducer, on} from '@ngrx/store';
 export const productReducer = createReducer(
   { showProductCode: true },
   on(createAction('[Product] Toggle Product Code'), state => {
-    console.log('original state: ' + JSON.stringify(state));
     return {
       ...state,
       showProductCode: !state.showProductCode
